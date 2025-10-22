6407d53725dc793edd7db329d3096b5ce434f627
henrl <henrylau111@yahoo.com.au>
2025-10-22 22:05:46 +1100

    optimize file read

--- index.ts
+++ index.ts
@@ -4,22 +4,25 @@ import fs from 'fs/promises';
 const host = 'localhost';
 const port = 3000;
 
+let indexFile;
+
 const listener = (req, res) => {
-    fs.readFile(__dirname + "/index.html")
-        .then(contents => {
-            res.setHeader('Content-Type', "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        })
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile);
 };
 
 const server = http.createServer(listener);
-server.listen(port, host, () => {
-    console.log(`server running on http://${host}:${port}`)
-});
+
+fs.readFile(__dirname + "index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`server running on http://${host}:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error('error');
+        process.exit(1);
+    })
 
