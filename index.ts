394ad95c9d73a540a3dae577d3acf98146fa9e96
henrl <henrylau111@yahoo.com.au>
2025-10-22 22:16:50 +1100

    routing

--- index.ts
+++ index.ts
@@ -4,25 +4,32 @@ import fs from 'fs/promises';
 const host = 'localhost';
 const port = 3000;
 
-let indexFile;
-
 const listener = (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(indexFile);
+    let filename = __dirname + "/404.html";
+    switch (req.url) {
+        case "/":
+            filename = __dirname + "/index.html";
+            break;
+        case "/about":
+            filename = __dirname + "/about.html";
+            break;
+        case "/contact-me":
+            filename = __dirname + "/contact-me.html";
+            break;
+        default:
+            filename = __dirname + "/404.html";
+    }
+    fs.readFile(filename)
+        .then((contents) => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        });
 };
 
 const server = http.createServer(listener);
 
-fs.readFile(__dirname + "index.html")
-    .then(contents => {
-        indexFile = contents;
-        server.listen(port, host, () => {
-            console.log(`server running on http://${host}:${port}`);
-        });
-    })
-    .catch(err => {
-        console.error('error');
-        process.exit(1);
-    })
+server.listen(port, host, () => {
+    console.log(`Server running on http://${host}:${port}`);
+})
 
