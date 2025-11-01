26548e8e55de21f27e1ea6c8f935d268410bdece
henrl <henrylau111@yahoo.com.au>
2025-11-01 12:47:42 +1100

    use env vars

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
+import { config } from 'dotenv';
 
+config();
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("hello world");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error: any) => {
     if (error) {
         throw error;
