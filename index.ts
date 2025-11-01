2ed1070f9ff70b15c3ea8b2f77cc6ade4b065f2e
henrl <henrylau111@yahoo.com.au>
2025-11-01 12:40:21 +1100

    redo

--- index.ts
+++ index.ts
@@ -4,32 +4,4 @@ import fs from 'fs/promises';
 const host = 'localhost';
 const port = 3000;
 
-const listener = (req, res) => {
-    let filename = __dirname + "/404.html";
-    switch (req.url) {
-        case "/":
-            filename = __dirname + "/index.html";
-            break;
-        case "/about":
-            filename = __dirname + "/about.html";
-            break;
-        case "/contact-me":
-            filename = __dirname + "/contact-me.html";
-            break;
-        default:
-            filename = __dirname + "/404.html";
-    }
-    fs.readFile(filename)
-        .then((contents) => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        });
-};
-
-const server = http.createServer(listener);
-
-server.listen(port, host, () => {
-    console.log(`Server running on http://${host}:${port}`);
-})
 
